47db860b6ec36c82fcaed84f2bd540ae561484bf
omar2550 <omr222000@gmail.com>
2025-05-10 03:07:45 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,156 +16,119 @@ lettersArray.forEach((letter) => {
 });
 
 // category
-const words = {
-  programming: [
-    "php",
-    "javascript",
-    "go",
-    "scala",
-    "fortran",
-    "r",
-    "mysql",
-    "python",
-  ],
-  movies: [
-    "Prestige",
-    "Inception",
-    "Parasite",
-    "Interstellar",
-    "Whiplash",
-    "Memento",
-    "Coco",
-    "Up",
-  ],
-  people: [
-    "Albert Einstein",
-    "Hitchcock",
-    "Alexander",
-    "Cleopatra",
-    "Mahatma Ghandi",
-  ],
-  countries: [
-    "Syria",
-    "Palestine",
-    "Yemen",
-    "Egypt",
-    "Bahrain",
-    "Qatar",
-    "Argentina",
-    "Belgium",
-    "Brazil",
-    "China",
-    "France",
-    "Germany",
-    "Iceland",
-    "India",
-    "Japan",
-    "Jordan",
-    "Iraq",
-    "Algeria",
-    "Libya",
-  ],
-};
 
-let allKeys = Object.keys(words);
+async function getWords() {
+  let words = await fetch("category.json");
+  words = await words.json();
+  try {
+    let allKeys = Object.keys(words);
 
-let randemPropNumber = Math.floor(Math.random() * allKeys.length);
+    let randemPropNumber = Math.floor(Math.random() * allKeys.length);
 
-let randemPropName = allKeys[randemPropNumber];
+    let randemPropName = allKeys[randemPropNumber];
 
-let randomPropValue = words[randemPropName];
+    let randomPropValue = words[randemPropName];
 
-let randemValueNumber = Math.floor(Math.random() * randomPropValue.length);
+    let randemValueNumber = Math.floor(Math.random() * randomPropValue.length);
 
-let randomWord = randomPropValue[randemValueNumber];
+    let randomWord = randomPropValue[randemValueNumber];
 
-document.querySelector(".category span").innerHTML = randemPropName;
+    document.querySelector(".category span").innerHTML = randemPropName;
 
-// Letters Guess
-let letterGuessContainer = document.querySelector(".letters-guess");
+    // Letters Guess
+    let letterGuessContainer = document.querySelector(".letters-guess");
 
-let lettersAndSpace = Array.from(randomWord);
+    let lettersAndSpace = Array.from(randomWord);
 
-lettersAndSpace.forEach((letter) => {
-  let span = document.createElement("span");
+    lettersAndSpace.forEach((letter) => {
+      let span = document.createElement("span");
 
-  if (letter === " ") {
-    span.className = "with-space";
-  }
+      if (letter === " ") {
+        span.className = "with-space";
+      }
 
-  letterGuessContainer.appendChild(span);
-});
+      letterGuessContainer.appendChild(span);
+    });
 
-// Handle Clicking on litters
+    // Handle Clicking on litters
 
-let guessSpans = document.querySelectorAll(".letters-guess span");
+    let guessSpans = document.querySelectorAll(".letters-guess span");
 
-let wrongCount = 0;
+    let wrongCount = 0;
 
-let hangDraw = document.querySelector(".hangman-draw");
-let clickedLetterArray = [];
+    let hangDraw = document.querySelector(".hangman-draw");
+    let clickedLetterArray = [];
 
-document.addEventListener("click", (e) => {
-  let theStatus = false;
+    document.addEventListener("click", (e) => {
+      let theStatus = false;
 
-  if (e.target.className === "letter-box") {
-    e.target.classList.add("clicked");
+      if (e.target.className === "letter-box") {
+        e.target.classList.add("clicked");
 
-    let clickedLetter = e.target.innerHTML.toLowerCase();
+        let clickedLetter = e.target.innerHTML.toLowerCase();
 
-    lettersAndSpace.forEach((wordletter, i) => {
-      if (wordletter.toLowerCase() === clickedLetter) {
-        theStatus = true;
-        clickedLetterArray[i] = clickedLetter;
+        lettersAndSpace.forEach((wordletter, i) => {
+          if (wordletter.toLowerCase() === clickedLetter) {
+            theStatus = true;
+            clickedLetterArray[i] = clickedLetter;
 
-        guessSpans.forEach((span, spanI) => {
-          if (spanI === i) {
-            span.innerHTML = clickedLetter;
+            guessSpans.forEach((span, spanI) => {
+              if (spanI === i) {
+                span.innerHTML = clickedLetter;
+              }
+            });
           }
         });
+
+        if (!theStatus) {
+          wrongCount++;
+
+          hangDraw.classList.add(`wrong-${wrongCount}`);
+
+          if (wrongCount === 8) {
+            endGame(wrongCount);
+            lettersContainer.classList.add("finshed");
+          }
+        }
+
+        if (
+          clickedLetterArray.join("") ===
+          lettersAndSpace.join("").split("").join("").toLowerCase()
+        ) {
+          endGame(wrongCount);
+          lettersContainer.classList.add("finshed");
+        }
       }
     });
 
-    if (!theStatus) {
-      wrongCount++;
-
-      hangDraw.classList.add(`wrong-${wrongCount}`);
-
-      if (wrongCount === 8) {
-        endGame(wrongCount);
-        lettersContainer.classList.add("finshed");
+    function endGame(counter) {
+      let div = document.createElement("div");
+      let divWin = document.createElement("div");
+      if (counter < 8) {
+        div.append(document.createTextNode("Congratulation, You Win!"));
+        divWin.append(
+          document.createTextNode(`Number Of Fails Is: ${counter}`)
+        );
+        div.appendChild(divWin);
+        if (counter < 4) {
+          let pro = document.createElement("div");
+          pro.append(document.createTextNode(`You Are Pro`));
+          div.appendChild(pro);
+        } else {
+          let noob = document.createElement("div");
+          noob.append(document.createTextNode(`You Are Noob`));
+          div.appendChild(noob);
+        }
+      } else {
+        div.append(
+          document.createTextNode(`Game Over, The Word Is ${randomWord}`)
+        );
       }
+      div.classList.add("popup");
+      document.body.append(div);
     }
-
-    if (
-      clickedLetterArray.join("") ===
-      lettersAndSpace.join("").split("").join("").toLowerCase()
-    ) {
-      endGame(wrongCount);
-      lettersContainer.classList.add("finshed");
-    }
+  } catch (e) {
+    console.log(Error(`Data Not Found ${e}`));
   }
-});
-
-function endGame(counter) {
-  let div = document.createElement("div");
-  let divWin = document.createElement("div");
-  if (counter < 8) {
-    div.append(document.createTextNode("Congratulation, You Win!"));
-    divWin.append(document.createTextNode(`Number Of Fails Is: ${counter}`));
-    div.appendChild(divWin);
-    if (counter < 4) {
-      let pro = document.createElement("div");
-      pro.append(document.createTextNode(`You Are Pro`));
-      div.appendChild(pro);
-    } else {
-      let noob = document.createElement("div");
-      noob.append(document.createTextNode(`You Are Noob`));
-      div.appendChild(noob);
-    }
-  } else {
-    div.append(document.createTextNode(`Game Over, The Word Is ${randomWord}`));
-  }
-  div.classList.add("popup");
-  document.body.append(div);
 }
+getWords();
